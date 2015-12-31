d35a99e4c5745dd5e185877329efc84fc9230ec6
dhytxz <hding4@gmail.com>
2015-12-31 13:44:49 -0600

    Fix Crawler test

--- src/test/java/org/hding/StockCrawler/StockCrawlerTest.java
+++ src/test/java/org/hding/StockCrawler/StockCrawlerTest.java
@@ -1,6 +1,8 @@
 package org.hding.StockCrawler;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.Assert;
 
 import org.hding.StockCrawler.util.Utils;
 import org.hding.StockCrawler.base.Date;
@@ -10,17 +12,16 @@ import org.hding.StockCrawler.base.StockInfo;
  * Created by hding on 12/31/15.
  */
 public class StockCrawlerTest extends TestCase {
-    public StockCrawlerTest( String testName )
-    {
-        super( testName );
-    }
+
+    @Test(timeout = 20000)
     public void testStockCrawler() throws Exception {
         StockInfo stock = new StockInfo(Utils.stockType.SZ, 2108);
         Date start = new Date(2015, 9, 9);
-        Date end = new Date(2015, 9, 9);
+        Date end = new Date(2015, 9, 10);
         Crawler sc = new Crawler(stock);
         sc.setStartDate(start);
         sc.setEndDate(end);
         sc.run();
+        Assert.assertTrue(true);
     }
 }
